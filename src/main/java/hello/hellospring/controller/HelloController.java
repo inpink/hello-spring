b56e618b2fb30e0f70a3474a308d2d9c63623dc1
inpink <inpink@kakao.com>
2022-12-20 01:16:50 +0900

    second commit

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,4 +1,14 @@
 package hello.hellospring.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller //Controller를 만들거면 @Controller를 넣어줘야 함.
 public class HelloController {
+    @GetMapping("hello") // /hello라는 url으로 연결 (hello적어줘야 함)
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!"); //data라는 이름으로 "hello!!"라는 값을 전달한다!
+        return "hello";  //hello.html으로 연결(hello적어줘야 함)
+    }
 }
